956b84239fd4f234d1e5a3ada4bffb41c5886bc6
duh-h <cadugon1000@yahoo.com>
2023-09-18 16:31:49 -0300

    Adicionar arquivos de projeto.

---
+++ caNomeProprio/NomeProprio.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace caNomeProprio
+{
+    internal class NomeProprio
+    {
+        string nome_completo { get; set; }
+        string[] nome_paper { get; set; }
+
+
+        public NomeProprio(string nome_completo)
+        {
+            this.nome_completo = nome_completo;
+            this.nome_paper = nome_completo.Split(' ');
+        }
+        public void ImprimeNomePaper()
+        {
+            Console.Write(nome_paper[nome_paper.Length - 1] + ", ");
+            for (int i = 0; i < nome_paper.Length - 1; i++)
+            {
+                if ((i == nome_paper.Length - 2) & (nome_paper.Length != 2) )
+                    Console.Write(nome_paper[i][0] + ".");
+                else
+                    Console.Write(nome_paper[i] + " ");
+            }
+        }
+
+
+    }
+}
